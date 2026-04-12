6411073e26ecf0299647a09393bacdd1763a52e2
Jack Burky <76707499+jsburky@users.noreply.github.com>
2026-04-12 11:43:13 -0400

    Made initial prototype files

---
+++ syncinary/lib/pages/flight_search.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+
+class flight_search extends StatefulWidget {
+  const flight_search({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+  
+  @override
+  State<flight_search> createState() => _flightState();
+}
+
+class _flightState extends State<flight_search> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+
+    
+  }
+
+  void _resetCounter() {
+      setState(() {
+        _counter = 0;
+      });
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 20,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+
+            ElevatedButton(
+              onPressed: _resetCounter, 
+              child: const Text('Reset Counter'),
+            ),
+          ],  
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
